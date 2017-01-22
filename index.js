dbb4bbe21dc1359005cd4a16b0718040da19946c
August Valera <4U6U57@gmail.com>
2017-01-21 16:58:05 -0800

    Adds index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyATqD4uLdQ0WF5Ca1uytQ7jFRYLrjLJOHs",
+  authDomain: "rackin-e70be.firebaseapp.com",
+  databaseURL: "https://rackin-e70be.firebaseio.com",
+  storageBucket: "rackin-e70be.appspot.com",
+  messagingSenderId: "1074873337266"
+};
+firebase.initializeApp(config);
+
+var app = angular.module('RackinApp', ['ngMaterial','firebase']);
